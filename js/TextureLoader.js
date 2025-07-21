9487a952d722c674388d032c66692fdcf9d3ee1a
Lilithe Bowman <lilithe.bowman@gmail.com>
2025-07-21 09:59:24 -0400

    Missing error & CORS handling in loadTexture
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- js/TextureLoader.js
+++ js/TextureLoader.js
@@ -4,19 +4,24 @@ class TextureLoader {
 		this.gl = gl;
 	}
 
-	async loadTexture(url) {
-		const gl = this.gl;
-		const image = new Image();
-		image.src = url;
-		await new Promise(res => { image.onload = res; });
-		const texture = gl.createTexture();
-		gl.bindTexture(gl.TEXTURE_2D, texture);
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-		return texture;
+    async loadTexture(url) {
+        const gl = this.gl;
+        const image = new Image();
+        image.crossOrigin = 'anonymous';
+        image.src = url;
+        await new Promise((res, rej) => {
+            image.onload = res;
+            image.onerror = () => rej(new Error(`Failed to load texture: ${url}`));
+        });
+        const texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        return texture;
+    }
 	}
 }
 
